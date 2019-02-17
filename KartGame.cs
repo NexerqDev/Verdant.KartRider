e8cc2fe3f1c409b9d706685670572fd0073b4e33
Nicholas Tay <nkt@outlook.kr>
2019-02-17 23:09:07 +1100

    Initial working

---
+++ KartGame.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Verdant.KartRider
+{
+    public class KartGame
+    {
+        public NaverAccount Account;
+        public string MainCharName;
+
+        private string ngmPath;
+
+        private HttpClient webClient => Account.WebClient;
+
+        public KartGame(NaverAccount account)
+        {
+            Account = account;
+        }
+
+        public async Task Init()
+        {
+            // Find NGM location
+            ngmPath = Tools.GetNgmPath();
+            if (ngmPath == null)
+                throw new VerdantException.GameNotFoundException();
+
+            if (!(await getKart()))
+                throw new VerdantException.ChannelingRequiredException();
+        }
+
+        public async Task Channel()
+        {
+            await channeling();
+            Account.SaveCookies();
+
+            if (!(await getKart()))
+                throw new Exception("no");
+        }
+
+        private const string LAUNCH_LINE = "-dll:platform.nexon.com/NGM/Bin/NGMDll.dll:1 -locale:KR -mode:launch -game:73985:0 -token:'{0}' -passarg:'null' -timestamp:{1} -position:'naverWeb' -service:6 -architectureplatform:'none'";
+        public async Task Start(bool firstTry = true)
+        {
+            // last minute cookies need to be get here - MSGENC and what not
+            int ts = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds; // timestamp: https://stackoverflow.com/questions/17632584/how-to-get-the-unix-timestamp-in-c-sharp
+
+            // last min update sesh
+            Debug.WriteLine("updating session");
+            var sesRes = await webClient.GetAsync("https://sso.nexon.game.naver.com/Ajax/Default.aspx?_vb=UpdateSession");
+
+            if (Account.Preloaded)
+            {
+                await Account.WebClient.GetAsync("http://nxgamechanneling.nexon.game.naver.com/login/loginproc.aspx?gamecode=589824");
+            }
+
+            string npp = Account.Cookies.GetCookies(new Uri("http://nexon.game.naver.com"))["NPP"].Value;
+
+            Debug.WriteLine("launching");
+            string args = String.Format(LAUNCH_LINE, npp, ts.ToString());
+            var psi = new ProcessStartInfo(ngmPath, args);
+            Process.Start(psi);
+        }
+
+        private async Task channeling()
+        {
+            await webClient.GetAsync("http://api.game.naver.com/js/jslib.nhn?gameId=P_PN000222&needReload=true");
+            await webClient.GetAsync("http://kart.nexon.game.naver.com/main/index.aspx");
+        }
+
+        private Regex charRepRegex = new Regex("<p class=\"user_name\"><strong>(.+?)</strong>");
+        private async Task<bool> getKart()
+        {
+            HttpResponseMessage res = await webClient.GetAsync("http://kart.nexon.game.naver.com/main/index.aspx");
+            res.EnsureSuccessStatusCode();
+
+            string data = await res.Content.ReadAsStringAsync();
+            if (data.Contains("javascript:doLogin()"))
+                return false;
+
+            Match m = charRepRegex.Match(data);
+            MainCharName = "(Unknown)";
+            if (m.Success)
+                MainCharName = m.Groups[1].Value;
+
+            return true;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Verdant.KartRider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NaverAccount account;
+        private KartGame Kart;
+
+        public MainWindow(NaverAccount acc)
+        {
+            InitializeComponent();
+            account = acc;
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Process.GetProcessesByName("KartRider").Length > 0
+             || Process.GetProcessesByName("NGM").Length > 0)
+            {
+                var mbr = MessageBox.Show("You are already playing Kart Rider! Continue?", "Kart", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                if (mbr == MessageBoxResult.No)
+                {
+                    Close();
+                    return;
+                }
+            }
+
+            Kart = new KartGame(account);
+
+            try
+            {
+                await Kart.Init();
+            }
+            catch (VerdantException.GameNotFoundException)
+            {
+                MessageBox.Show("Looks like we couldn't find your installation of Kart Rider! Make sure you have Korean Kart Rider + NGM installed properly!");
+                Close();
+                return;
+            }
+            catch (VerdantException.ChannelingRequiredException)
+            {
+                statusLabel.Content = "Loading... (channeling)";
+                try
+                {
+                    await Kart.Channel();
+                }
+                catch
+                {
+                    MessageBox.Show("Error channeling with Nexon and your Naver account. Please try again later.");
+                    Close();
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error connecting to Kart Rider, please try again later.");
+                Close();
+                return;
+            }
+
+            statusLabel.Content = "Logged in as: " + Kart.MainCharName;
+            startButton.IsEnabled = true;
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            startButton.IsEnabled = false;
+            try
+            {
+                await Kart.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error starting game...\n\n" + ex.ToString());
+            }
+            Close();
+        }
+    }
+}
